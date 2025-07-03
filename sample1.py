0cacbfee5edfcdfc159d381819759d0d6fc742bd
thaneshkumran <thaneshr42@gmail.com>
2025-07-03 18:53:33 +0530

    squash commit changes

--- sample1.py
+++ sample1.py
@@ -5,3 +5,13 @@ else:
     print("hello world!")
 print("hello world!")
 print("file modified from feature branch")
+#frist commit
+print("frist commit from feature branch")
+#second commit
+print("second commit from feature branch")
+#third commit
+print("third commit from feature branch")
+#fourth commit
+print("fourth commit from feature branch")
+#fifth commit
+print("fifth commit from feature branch")
